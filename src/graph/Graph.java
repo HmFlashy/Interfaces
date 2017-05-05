3ed2c5eb30f705f66d1449f70460190470f36470
Clm-Roig <kevin.hassan@etu.umontpellier.fr>
2017-05-05 17:04:20 +0200

    Ouai

--- src/graph/Graph.java
+++
@@ -1,9 +0,0 @@
-package graph;
-
-interface Graph {
-	void addVertex(Vertex v);
-	void addUndirected(Vertex v1, Vertex v2);
-	void removeVertex(Vertex v);
-	void removeEdge(Edge e);
-	void addEdge(Edge e);
-}
--- src/graph/UndirectEdge.java
+++
@@ -1,8 +0,0 @@
-package graph;
-
-class UndirectEdge extends Edge {
-	
-	public UndirectEdge(Vertex v1, Vertex v2){
-		super(v1, v2);
-	}
-}
